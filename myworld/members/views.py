4fcac852818467fcd30276f9e590d55a76652340
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-10-30 11:58:57 -0600

    Changing Data in Database
    Using the python shell to add, update, and remove members from database.

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def members(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
