35b791aad58630f39ba54e9a4e286eac5790531a
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-01 08:59:29 -0600

    Added PizzaOrder class to models.py
    Also migrated changes

--- myworld/newApp/models.py
+++ myworld/newApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class PizzaOrder(models.Model):
+    HAWAIIAN = 'HI'
+    PEPPERONI = 'PE'
+    CHEESE = 'CH'
+    MEAT = 'MT'
+    PIZZA_STYLE_CHOICES = [
+        (HAWAIIAN, 'Hawaiian'),
+        (PEPPERONI, 'Pepperoni'),
+        (CHEESE, 'Cheese'),
+        (MEAT, 'Meat')
+    ]
+
+    LARGE = 14
+    MEDIUM = 12
+    SMALL = 8
+    PIZZA_SIZE_CHOICES = [
+        (LARGE, 'Large'),
+        (MEDIUM, 'Medium'),
+        (SMALL, 'Small'),
+    ]
+    customerName = models.CharField(max_length=50)
+    timeOfOrder = models.DateField(auto_now_add=True)
+    size = models.IntegerField(choices=PIZZA_SIZE_CHOICES)
+    style = models.CharField(max_length=2, choices=PIZZA_STYLE_CHOICES)
